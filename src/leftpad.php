afabc1016304b2dd6c89d0e56d07a528668188f1
ryanwinchester <fungku@gmail.com>
2016-04-25 11:10:58 -0700

    PSR-2

--- src/leftpad.php
+++ src/leftpad.php
@@ -7,7 +7,8 @@ if (!function_exists('leftpad')) {
      * @param   string   $filler
      * @return  string
      */
-    function leftpad($string, $length, $filler = ' ') {
+    function leftpad($string, $length, $filler = ' ')
+    {
         return str_pad($string, $length, $filler, STR_PAD_LEFT);
     }
 }

de1ef4a56123d0a1779bb7426cdc47ee44acff14
Alex Plekhanov <alex@plekhanov.us>
2019-09-18 22:14:45 +0200

    Updated for php 7.1

--- src/leftpad.php
+++ src/leftpad.php
@@ -1,13 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 if (!function_exists('leftpad')) {
     /**
-     * @param   string   $string
-     * @param   int      $length
-     * @param   string   $filler
+     * @param string $string
+     * @param int $length
+     * @param string $filler
      * @return  string
      */
-    function leftpad($string, $length, $filler = ' ')
+    function leftpad(string $string, int $length, string $filler = ' '): string
     {
         return str_pad($string, $length, $filler, STR_PAD_LEFT);
     }
--- tests/LeftPadTest.php
+++ tests/LeftPadTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 
 class LeftPadTest extends TestCase
@@ -9,7 +11,7 @@ class LeftPadTest extends TestCase
         $this->assertEquals('  foo', leftpad('foo', 5));
         $this->assertEquals('foobar', leftpad('foobar', 6));
         $this->assertEquals('foobar', leftpad('foobar', 3));
-        $this->assertEquals('01', leftpad(1, 2, 0));
-        $this->assertEquals('-1', leftpad(1, 2, '-'));
+        $this->assertEquals('01', leftpad('1', 2, '0'));
+        $this->assertEquals('-1', leftpad('1', 2, '-'));
     }
 }

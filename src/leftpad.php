84614c39ab666aed97222c3bde1529d8592e7a3a
Alex Plekhanov <alex@plekhanov.io>
2023-01-05 00:27:33 +0100

    Removed redundant PHPDoc

--- src/leftpad.php
+++ src/leftpad.php
@@ -3,12 +3,6 @@
 declare(strict_types=1);
 
 if (!function_exists('leftpad')) {
-    /**
-     * @param string $string
-     * @param int $length
-     * @param string $filler
-     * @return  string
-     */
     function leftpad(string $string, int $length, string $filler = ' '): string
     {
         return str_pad($string, $length, $filler, STR_PAD_LEFT);
